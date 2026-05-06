ab9e6e73e4238f759465a2918280342a1cae4c8c
Sebastian Silvestrean <sebsilve@shi-r1-p2.42belgium.be>
2026-05-06 16:58:48 +0200

    need makefile et readme

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,12 +6,29 @@
 /*   By: sebsilve <seb.silves@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/05 00:34:36 by sebsilve          #+#    #+#             */
-/*   Updated: 2026/05/05 15:43:41 by sebsilve         ###   ########.fr       */
+/*   Updated: 2026/05/06 16:35:48 by sebsilve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	
+	t_list	*current;
+	t_list	*f_lst;
+	t_list	*new;
+
+	current = lst;
+	f_lst = NULL;
+	while (current)
+	{
+		new = ft_lstnew(f(current->content));
+		if (!new)
+		{
+			ft_lstclear(&f_lst, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&f_lst, new);
+		current = current->next;
+	}
+	return (f_lst);
 }
